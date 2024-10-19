cee2a1cbfedaaf6829022dabca38d4cde50fe72b
jacobleazott <jacob.leazott@gmail.com>
2024-10-19 11:29:51 -0600

    Rewored file structure and tree code

--- tree/src/Tree.py
+++ tree/src/Tree.py
@@ -1,30 +1,25 @@
 # ╔════╦══════╦══════╦══════╦══════╦══════╦══════╦══════╦═══════╦══════╦══════╦══════╦══════╦══════╦══════╦══════╦════╗
 # ║  ╔═╩══════╩══════╩══════╩══════╩══════╩══════╩══════╩═══════╩══════╩══════╩══════╩══════╩══════╩══════╩══════╩═╗  ║
 # ╠══╣                                                                                                             ╠══╣
-# ║  ║    SETTINGS AND CONSTANTS                   CREATED: 2024-10-17          https://github.com/jacobleazott    ║  ║
+# ║  ║    CHRISTMAS TREE                           CREATED: 2024-10-19          https://github.com/jacobleazott    ║  ║
 # ║══║                                                                                                             ║══║
 # ║  ╚═╦══════╦══════╦══════╦══════╦══════╦══════╦══════╦═══════╦══════╦══════╦══════╦══════╦══════╦══════╦══════╦═╝  ║
 # ╚════╩══════╩══════╩══════╩══════╩══════╩══════╩══════╩═══════╩══════╩══════╩══════╩══════╩══════╩══════╩══════╩════╝
 # ════════════════════════════════════════════════════ DESCRIPTION ════════════════════════════════════════════════════
-# 
+# This file defines what we effects we actually want to run on the Christmas Tree and for how long. 
 # ═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
-import board
-import neopixel
-from dataclasses import dataclass
+import time
+from Effects import LEDEffects
 
+def main():
+    led_effects = LEDEffects()
+    while True:
+        led_effects.run_effect_for_x_seconds(led_effects.rainbow, duration=5, args=(0, 0.01, 0.75))
+        led_effects.run_effect_for_x_seconds(led_effects.solid_color_rainbow, duration=5)
+        
 
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-DESCRIPTION: Class that holds all of our user configurable settings/ constants for the project.
-"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-@dataclass(frozen=True)
-class SettingsClass:
-    
-    NUM_LEDS: int  = 650
-    LED_PIN = board.D18
-    LED_ORDER: str = neopixel.GRB
-    LED_BRIGHTNESS: float = 0.5
+if __name__ == "__main__":
+    main()
 
-
-Settings = SettingsClass()
 
 # FIN ═════════════════════════════════════════════════════════════════════════════════════════════════════════════════
---
+++ tree/src/helpers/Settings.py
@@ -0,0 +1,43 @@
+# ╔════╦══════╦══════╦══════╦══════╦══════╦══════╦══════╦═══════╦══════╦══════╦══════╦══════╦══════╦══════╦══════╦════╗
+# ║  ╔═╩══════╩══════╩══════╩══════╩══════╩══════╩══════╩═══════╩══════╩══════╩══════╩══════╩══════╩══════╩══════╩═╗  ║
+# ╠══╣                                                                                                             ╠══╣
+# ║  ║    SETTINGS AND CONSTANTS                   CREATED: 2024-10-17          https://github.com/jacobleazott    ║  ║
+# ║══║                                                                                                             ║══║
+# ║  ╚═╦══════╦══════╦══════╦══════╦══════╦══════╦══════╦═══════╦══════╦══════╦══════╦══════╦══════╦══════╦══════╦═╝  ║
+# ╚════╩══════╩══════╩══════╩══════╩══════╩══════╩══════╩═══════╩══════╩══════╩══════╩══════╩══════╩══════╩══════╩════╝
+# ════════════════════════════════════════════════════ DESCRIPTION ════════════════════════════════════════════════════
+# This file solely holds our SettingsClass which is just a collection of "configurable" values that a user of the
+#   project may want to change. It offers a single location for all files to reference. It utilizes a frozen dataclass
+#   which means when we instantiate it as 'Settings' no inheriter can ever change these values. In this way we make a 
+#   common location for immutable settings during runtime. I still leave the discretion to include constants in
+#   individual files for 'magic' numbers that should NEVER change and shouldn't be 'easily' changed through these 
+#   configurations.
+# ═════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
+import board
+import neopixel
+from dataclasses import dataclass
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+DESCRIPTION: Class that holds all of our user configurable settings/ constants for the project.
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+@dataclass(frozen=True)
+class SettingsClass:
+    # LED Strip Settings
+    LED_PIN: int        = 18        # GPIO pin connected to the pixels (must support PWM or PCM)
+    NUM_LEDS: int       = 650       # Number of LED pixels
+    LED_BRIGHTNESS: int = 128       # Brightness of LEDs (0-255)
+    LED_FREQ_HZ: int    = 800000    # LED signal frequency in hertz (usually 800kHz)
+    LED_DMA: int        = 10        # DMA channel to use for generating signal (try 10)
+    LED_INVERT: bool    = False     # True to invert the signal (when using NPN transistor level shift)
+    LED_CHANNEL: int    = 0         # Channel (default is 0)
+    LED_ORDER: str      = "GRB"     # Pixel color order (typically 'GRB')
+    
+    # Internal Implementation Settings
+    UPDATE_QUEUE_SIZE: int = 60    # Number of updates we can "preprocess" before waiting to calculate further
+
+    # Logging Settings
+    FUNCTION_ARG_LOGGING_LEVEL: int = 15
+
+Settings = SettingsClass()
+
+# FIN ═════════════════════════════════════════════════════════════════════════════════════════════════════════════════
