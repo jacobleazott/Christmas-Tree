6871e0635d4052c15780d698affd6efefc7195f6
jacobleazott <jacob.leazott@gmail.com>
2024-10-25 00:11:41 -0600

    Removed bad folder

--- Laptop/plot_3d.py
+++
@@ -1,9 +0,0 @@
-
-file = open(f'auto_coordinates.txt', 'r')
-write_File = open(f'auto_corrected_coordinates.txt', 'w')
-coord = file.readline().split()
-
-write_File.write(f"{int(coord[0]) - 350} {int(coord[1]) + 1} {int(coord[2]) + 13}/n")
-
-file.close()
-write_File.close()
